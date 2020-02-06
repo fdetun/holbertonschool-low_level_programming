87ed5580d6afc26358d28e9552f76f08df13c550
Foued Lamine <fouedads@gmail.com>
2020-02-06 19:40:21 +0100

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-  main block
+ * Return: 0
+ */
+int main(void)
+{
+int f = 48;
+int l = 44;
+int e = 32;
+while (f <= 56)
+{
+putchar(f);
+putchar(l);
+putchar(e);
+f++;
+}
+putchar(57);
+return (0);
+}
