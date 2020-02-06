e33d8ad20a9fa82ee32a43a4f539e612cfe91e5a
Foued Lamine <fouedads@gmail.com>
2020-02-06 19:54:54 +0100

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@ int main(void)
 int f = 48;
 int l = 44;
 int e = 32;
-while (f <= 56)
+while (f <= 57)
 {
 putchar(f);
+if (f <= 56)
+{
 putchar(l);
 putchar(e);
+}
 f++;
 }
-putchar(57);
+putchar('\n');
 return (0);
 }
