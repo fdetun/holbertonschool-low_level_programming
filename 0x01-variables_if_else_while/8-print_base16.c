f4b3f121ca517d77c66d80d0aed6eb0377a32923
Foued Lamine <fouedads@gmail.com>
2020-02-06 19:16:11 +0100

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-  main block
+ * Return: 0
+ */
+int main(void)
+{
+int f = 48;
+int l = 97;
+while (f <= 57)
+{
+putchar(f);
+f++;
+}
+while (l <= 102)
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
