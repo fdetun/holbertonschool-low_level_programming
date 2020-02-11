b23d90e9cb5ab5e29a522389ceaf5ec4ea6ce383
Foued Lamine <fouedads@gmail.com>
2020-02-11 18:15:19 +0100

    after fix

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,5 +9,8 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int f);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
 #endif
-
