64b16fcd114ea342bffe71e1197d4b6502bd8f09
Foued Lamine <fouedads@gmail.com>
2020-02-13 19:53:11 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,16 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; ++i)
+for (i = 1; i <= 100; i++)
 {
-if ((i % (3 * 5)) == 0)
-{
-printf("FizzBuzz ");
-}
-else if ((i % 5) == 0)
-{
-printf("Buzz ");
-}
-else if ((i % 3) == 0)
-{
-printf("Fizz ");
-}
-else
-{
-printf("%d ", i);
-}
+if (i != 1)
+printf(" ");
+if (i % 3 == 0)
+printf("Fizz");
+if (i % 5 == 0)
+printf("Buzz");
+if (i % 3 != 0 && i % 5 != 0)
+printf("%d", i);
 }
 printf("\n");
 return (0);
