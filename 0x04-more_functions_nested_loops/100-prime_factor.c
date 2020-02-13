76c5629118a3358bae5c90f2a22410f6aff2ab2a
Foued Lamine <fouedads@gmail.com>
2020-02-13 23:16:06 +0100

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - 100 prime factor
+* Return: 0
+*/
+int main(void)
+{
+long long int n;
+n = 612852475143;
+long long int a = 2, f;
+
+while (n != 0)
+{
+if (n % a != 0)
+a = a + 1;
+else
+{
+f = n;
+n = n / a;
+if (n == 1)
+{
+printf("%lli is the largest prime factor of 612852475143\n", f);
+break;
+}
+}
+}
+return (0);
+}
