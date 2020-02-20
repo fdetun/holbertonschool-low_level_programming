a58d089e7e4237266dc38cf831c935bf4bb04f17
Foued Lamine <fouedads@gmail.com>
2020-02-20 17:51:21 +0100

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - _strncpy  a string.
+ * @dest: char to define the string
+ * @src: char to define the string
+ * @n: integer to check
+ * Return: dest.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
