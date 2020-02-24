f823a9fc1cdc08e87ac3a50f0a01416ba42ebc46
Foued Lamine <fouedads@gmail.com>
2020-02-24 19:25:36 +0100

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i = 0;
 int j;
-while (s[i] != '\0')
+
+for (i = 0; s[i] != '\0'; i++)
 {
-i++;
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
