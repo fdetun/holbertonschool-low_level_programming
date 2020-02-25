0d01ef113813927ec5fac5503ebd1284b9cb022b
Foued Lamine <fouedads@gmail.com>
2020-02-25 20:58:27 +0100

    9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - set_string pointer to a char
+ * @s: pointer
+ * @to: string to change
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
