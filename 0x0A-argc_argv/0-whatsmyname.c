28f0810a5c4b174426468f4c2ea75f3d3016b8d0
Foued Lamine <fouedads@gmail.com>
2020-03-01 12:33:22 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main to an integer.
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
