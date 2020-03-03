c053d9bb19bcd2ab35fa1b7042890a3c78872427
Foued Lamine <fouedads@gmail.com>
2020-03-03 15:14:15 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *a;
+unsigned int i;
 
 if (size == 0)
 {
@@ -21,7 +22,7 @@ if (a == NULL)
 {
 return (NULL);
 }
-for (unsigned int i = 0; i < size; ++i)
+for (i = 0; i < size; ++i)
 {
 a[i] = c;
 }
