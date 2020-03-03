42c85f7825b74730262d053812687febe654beca
Foued Lamine <fouedads@gmail.com>
2020-03-03 16:42:38 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,25 +5,25 @@
 /**
  * _strdup - _strdup.
  * @str: variable
- * Return: 0 or 1
+ * Return: a Or NULL.
 */
 char *_strdup(char *str)
 {
 char *a;
 int j;
 
-a = malloc(8 * sizeof(char));
 if (str == NULL)
+{
+return (NULL);
+}
+a = malloc(8 * sizeof(char));
+if (a == NULL)
 {
 return (NULL);
 }
 for (j = 0; str[j] != '\0'; ++j)
 {
 a[j] = str[j];
-if (sizeof(str) > 8)
-{
-return (NULL);
-}
 }
 return (a);
 free(a);
