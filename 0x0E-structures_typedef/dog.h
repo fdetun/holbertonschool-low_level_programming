6c1635332a51c98ab0c46f284c390d14dd42b835
Foued Lamine <fouedads@gmail.com>
2020-03-09 23:20:04 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOGH_
+#define _DOGH_
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* _DOGH_ */
