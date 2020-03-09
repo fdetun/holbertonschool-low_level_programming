a811d51130b8cac20e2a4bd6c89db227b1d8e860
Foued Lamine <fouedads@gmail.com>
2020-03-09 18:42:27 +0100

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef _3_FUNCTOIN_LIKE_MACRO_
 #define _3_FUNCTOIN_LIKE_MACRO_
-#define ABS(x) ((x) >= (0) ? (x) : (-x))
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 #endif /* _3_FUNCTOIN_LIKE_MACRO_ */
