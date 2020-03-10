32ad971b1ecbafad1fa4136d630d5617d7f88e44
Foued Lamine <fouedads@gmail.com>
2020-03-10 15:24:55 +0100

    dog3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,11 @@ float age;
 char *owner;
 };
 
-typedef struct dog dog_f;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif /* _DOGH_ */
