a487022c63509fd9999b49e6cfb93738a2a44c8e
Foued Lamine <fouedads@gmail.com>
2020-03-13 17:18:56 +0100

    first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum_them_all func
+ * @n: unsigned int
+ * Return: 0 Or total
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int total = 0;
+unsigned int i;
+if (n == 0)
+{
+return (0);
+}
+va_list f1;
+va_start(f1, n);
+for (i = 0; i < n; ++i)
+{
+	total = total + va_arg(f1, unsigned int);
+}
+va_end(f1);
+return (total);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VARIADIC_FUN_
+#define _VARIADIC_FUN_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* _VARIADIC_FUN_ */
