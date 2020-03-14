e3a8b4fd06961e7632a425922cbb98a97a857b73
Foued Lamine <fouedads@gmail.com>
2020-03-14 12:25:20 +0100

    last fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-int total = 0;
+va_list f1;
 unsigned int i;
+int total = 0;
 if (n == 0)
 {
 return (0);
 }
-va_list f1;
 va_start(f1, n);
 for (i = 0; i < n; ++i)
 {
