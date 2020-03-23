5bf2747e139c52dbbb537882348c40790547e47e
Foued Lamine <fouedads@gmail.com>
2020-03-23 22:08:50 +0100

    commit n1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print_listint
+ * @h: list_t pointer
+ * Return: i
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
