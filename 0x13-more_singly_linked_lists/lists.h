3b31ce1cc2b4b10620ee433b1948469068490b46
Foued Lamine <fouedads@gmail.com>
2020-03-24 21:19:09 +0100

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,4 +26,5 @@ void free_listint(listint_t *head);
 int pop_listint(listint_t **head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 #endif /*_LISTS_*/
