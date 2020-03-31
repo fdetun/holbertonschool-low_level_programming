389a6b582004616d2a287dad389206c14907c95c
fdetun <fouedads@gmail.com>
2020-03-31 21:53:19 +0100

    fixing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,8 @@ int fd, lenght, a;
 lenght = strlen(text_content);
 if (!filename)
 return (-1);
-f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (f == -1)
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
 return (-1);
 if (text_content != NULL)
 a = write(fd, text_content, lenght);
