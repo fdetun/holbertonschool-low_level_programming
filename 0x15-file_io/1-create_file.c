5726b76e91877c1563e27e80720d235f9cfb9a27
fdetun <fouedads@gmail.com>
2020-03-31 21:49:00 +0100

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * create_file - create_file
+ * @filename: pointer of filename
+ * @text_content: pointerof string
+ * Return: -1 Or 1
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, lenght, a;
+
+lenght = strlen(text_content);
+if (!filename)
+return (-1);
+f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (f == -1)
+return (-1);
+if (text_content != NULL)
+a = write(fd, text_content, lenght);
+if (a == -1)
+return (-1);
+close(fd);
+return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,6 +7,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /*_HOLBERTONLIB_*/
