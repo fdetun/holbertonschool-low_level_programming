e11dc7274c4a3aaf60700cb1bcae78fbc126231f
fdetun <fouedads@gmail.com>
2020-05-02 13:14:14 +0100

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add_dnodeint
+ * @head: header
+ * @n: int
+ * Return: h
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *h;
+
+h = (dlistint_t *)malloc(sizeof(dlistint_t));
+if (h == NULL)
+return (h);
+h->n = n;
+h->next = *head;
+h->prev = NULL;
+*head = h;
+return (h);
+}
