7f38c0dba4de052de2d7ff624ef59b5bddf6210c
fdetun <fouedads@gmail.com>
2020-07-04 20:55:11 +0100

    fdetun

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+island_perimeter algo
+"""
+
+
+sm = 0
+
+
+def fdefunc(i, grid):
+    """fdefunc"""
+
+    global sm
+    listindex = []
+    for j in range(len(grid[i])):
+        if grid[i][j] == 1:
+            sm = sm + 3
+            listindex.append(j)
+            if grid[i][j + 1] == 1 and grid[i][j - 1] == 1:
+                sm = sm - 1
+            elif grid[i][j + 1] == 0 and grid[i][j - 1] == 0:
+                sm = sm + 1
+    return listindex
+
+
+def island_perimeter(grid):
+    """ island_perimeter """
+    global sm
+    for incr in range(len(grid)):
+        a = fdefunc(incr, grid)
+        for j in range(len(grid[incr])):
+            if grid[incr - 1][j] == 1 and j in a:
+                sm = sm - 2
+    return(sm)
