cd72a352d60c05e540492979c1afaf03c9607bf4
fdetun <fouedads@gmail.com>
2020-08-26 17:02:11 +0100

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,15 +13,13 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t i = 0;
 
-if (!array || size == 0)
+if (!array)
 return (-1);
 while (i < size)
 {
-if (array[i] == value)
-{
-return (i);
-}
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
 i++;
 }
 return (-1);
