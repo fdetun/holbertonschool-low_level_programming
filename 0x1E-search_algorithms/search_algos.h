9e3c2823e2ff14fe94712d2a10709d801e5eced8
fdetun <fouedads@gmail.com>
2020-08-26 16:35:52 +0100

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+ 
+#ifndef Search_algorithm
+#define Search_algorithm
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+#endif /* Search_algorithm */
